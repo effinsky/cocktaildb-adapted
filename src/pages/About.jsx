3d9f740e728fb323900dca2b0ff3d0867d2664a2
adi forkasiewicz <adiforka@gmail.com>
2021-03-22 11:00:34 +0100

    modified about text

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -10,9 +10,13 @@ const About = () => {
           a John Smilga. Thank you, John. Oddly enough, the CocktailDB is a real
           API you can use to get some potentially true info on drinks so you can
           get your drink on sooner so if that sounds interesting make sure you
-          click that subscribe button and the notification bell so you don't
-          miss any of my future videos ok one more youtube video and I'll freak
-          for real.
+          click all the buttons you find until you find a drink you like.
+
+          This was prepared mainly for fun and for job interviews. Both will have me
+          evolve this mini project into where we get stuff other than cocktail info, 
+          maybe music artist info instead, from my own backend. This text will then come 
+          from that backend as well. We will have forms to submit artist info and have
+          that sent over to the back end thru REST and persisted in... probably MongoDB.
         </p>
         <a
           target="_blank"
